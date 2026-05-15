33c7c058fa9c3ecfa481b3b02bd0bff197dbae48
tubarao123fr <tubarao123fr@gmail.com>
2026-05-15 15:32:30 +0200

    Fix PDF.js CDN

--- viewer.js
+++ viewer.js
@@ -3,7 +3,7 @@ const url = "https://drive.google.com/file/d/187DslUF9mt-9ZfTY6ioq26Yv4ubta1XD/v
 const container = document.getElementById("viewer");
 
 pdfjsLib.GlobalWorkerOptions.workerSrc =
-  "https://mozilla.github.io/pdf.js/build/pdf.worker.js";
+  "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js";
 
 pdfjsLib.getDocument(url).promise.then(pdf => {
 
