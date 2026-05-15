d0bd0acbd340afb2312adb7f07f1a4ae3ba27541
tubarao123fr <tubarao123fr@gmail.com>
2026-05-15 15:34:51 +0200

    Fix PDF.js CDN

--- viewer.js
+++ viewer.js
@@ -1,4 +1,4 @@
-const url = "https://drive.google.com/file/d/187DslUF9mt-9ZfTY6ioq26Yv4ubta1XD/view"
+const url = "https://drive.google.com/uc?export=download&id=187DslUF9mt-9ZfTY6ioq26Yv4ubta1XD";
 
 const container = document.getElementById("viewer");
 
@@ -6,25 +6,16 @@ pdfjsLib.GlobalWorkerOptions.workerSrc =
   "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js";
 
 pdfjsLib.getDocument(url).promise.then(pdf => {
-
   for (let pageNum = 1; pageNum <= pdf.numPages; pageNum++) {
     pdf.getPage(pageNum).then(page => {
-
       const scale = 1.5;
       const viewport = page.getViewport({ scale });
-
       const canvas = document.createElement("canvas");
       const ctx = canvas.getContext("2d");
-
       canvas.height = viewport.height;
       canvas.width = viewport.width;
-
       container.appendChild(canvas);
-
-      page.render({
-        canvasContext: ctx,
-        viewport: viewport
-      });
+      page.render({ canvasContext: ctx, viewport: viewport });
     });
   }
 });
