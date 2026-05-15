46a3811c98eb5622131c3c252a1388790cc827de
tubarao123fr <tubarao123fr@gmail.com>
2026-05-15 16:19:47 +0200

    Use GitHub Releases URL for PDF

--- viewer.js
+++ viewer.js
@@ -1,21 +1,46 @@
-const url = "https://github.com/tubarao123fr/tubarao.github.io/releases/download/v1.0/DRAGON.BALL.TOME.2.-Akira.Toriyama.pdf";
+const identifier = "dragon-ball-tome-2-akira-toriyama";
 
-const container = document.getElementById("viewer");
+fetch(`https://archive.org/metadata/${identifier}`)
+  .then(res => res.json())
+  .then(data => {
+    // cherche le premier PDF
+    const pdfFile = data.files.find(f => f.name.endsWith(".pdf"));
 
-pdfjsLib.GlobalWorkerOptions.workerSrc =
-  "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js";
+    if (!pdfFile) {
+      console.log("Aucun PDF trouvé");
+      return;
+    }
 
-pdfjsLib.getDocument(url).promise.then(pdf => {
-  for (let pageNum = 1; pageNum <= pdf.numPages; pageNum++) {
-    pdf.getPage(pageNum).then(page => {
-      const scale = 1.5;
-      const viewport = page.getViewport({ scale });
-      const canvas = document.createElement("canvas");
-      const ctx = canvas.getContext("2d");
-      canvas.height = viewport.height;
-      canvas.width = viewport.width;
-      container.appendChild(canvas);
-      page.render({ canvasContext: ctx, viewport: viewport });
+    const url =
+      `https://archive.org/download/${identifier}/${encodeURIComponent(pdfFile.name)}`;
+
+    console.log(url);
+
+    // charger le PDF avec pdf.js
+    pdfjsLib.GlobalWorkerOptions.workerSrc =
+      "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js";
+
+    const container = document.getElementById("viewer");
+
+    pdfjsLib.getDocument(url).promise.then(pdf => {
+      for (let pageNum = 1; pageNum <= pdf.numPages; pageNum++) {
+        pdf.getPage(pageNum).then(page => {
+          const scale = 1.5;
+          const viewport = page.getViewport({ scale });
+
+          const canvas = document.createElement("canvas");
+          const ctx = canvas.getContext("2d");
+
+          canvas.width = viewport.width;
+          canvas.height = viewport.height;
+
+          container.appendChild(canvas);
+
+          page.render({
+            canvasContext: ctx,
+            viewport
+          });
+        });
+      }
     });
-  }
-});
+  });
